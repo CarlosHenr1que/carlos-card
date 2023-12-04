9d6e23ee69395f3847c9487a70438cb9c2ac9af0
Carlos Henrique Matos Borges <CARLOS.COMPASSO@fleetcor.com.br>
2023-12-04 10:30:44 -0300

    Initial Commit

---
+++ CarlosCard/CarlosCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarlosCardApp.swift
+//  CarlosCard
+//
+//  Created by Carlos Henrique Matos Borges on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CarlosCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
